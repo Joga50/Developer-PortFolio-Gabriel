e07e30180e655928aa500a965b897f052c4b59ad
Joga50 <jose.gabriel.a12@gmail.com>
2023-02-01 20:39:47 -0500

    adding information and updating links

--- js/script.js
+++ js/script.js
@@ -3,17 +3,30 @@ window.addEventListener("scroll", function () {
   hedaer.classList.toggle("sticky", window.scrollY > 100);
 });
 
+let menu = document.querySelector("#menu-icon");
+let navlist = document.querySelector(".navlist");
+
+menu.onclick = () => {
+  menu.classList.toggle("bx-x");
+  navlist.classList.toggle("open");
+};
+window.onscroll = () => {
+  menu.classList.remove("bx-x");
+  navlist.classList.remove("open");
+};
 const portfolio = [
   {
-    img: "./assets/dofus-2022-06-29_12-20-02-Zabotage.png",
+    img: "./assets/socialMedia.png",
     name: "MERN, Social media app",
     description:
       "Complete social media app using MongoDB, Expresjs, Reactjs, Nodejs",
+    url: "React-JavaSpringboot-Fullstack-application",
   },
   {
-    img: "./assets/dofus-2022-07-01_07-46-48-Zabotage.png",
+    img: "./assets/springboot-CRUD.png",
     name: "CRUD, user register and login application",
-    description: "react and java springboot application with MySQL database",
+    description: "React and java springboot application with MySQL database",
+    url: "MERN-SocialMedia-Fullstack-Application",
   },
 ];
 
@@ -27,7 +40,7 @@ const html = portfolio
       
       <p>${item.description}</p>
       <div>
-     <a href="http//:github.com/joga50"> <i class='bx bxl-github'></i></a>
+     <a href="http://github.com/joga50/${item.url}" target="_blank"> <i class='bx bxl-github'></i></a>
      
      <a href="#" ><i class='bx bx-link-external'></i></a>
      </div>
